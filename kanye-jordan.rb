ba167e6402dcec5ef659a93ce72ec44c34b41789
Tom Armitage <tom@infovore.org>
2010-09-26 16:20:08 +0100

    Using a path prefix for when our server does things on absolute paths.

--- kanye-jordan.rb
+++ kanye-jordan.rb
@@ -6,11 +6,25 @@ require 'json'
 
 config = YAML.parse(File.read("creds.yml"))
 
-%w{consumer_key consumer_secret access_token access_token_secret}.each do |key|
+%w{consumer_key consumer_secret access_token access_token_secret path_prefix}.each do |key|
   Object.const_set(key.upcase, config["config"][key].value)
 end
 
-puts CONSUMER_KEY
-puts CONSUMER_SECRET
-puts ACCESS_TOKEN
-puts ACCESS_TOKEN_SECRET
+highestcount = File.read(PATH_PREFIX + "highest")
+
+oauth = Twitter::OAuth.new(CONSUMER_KEY, CONSUMER_SECRET)
+oauth.authorize_from_access(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+client = Twitter::Base.new(oauth)
+
+tweets = client.user_timeline(:screen_name => "kanyewest", :count => 50, :since_id => highestcount)
+
+tweets.reverse.each do |tweet|
+  newtext = "Liz Lemon, #{tweet.text}"
+  if newtext.size > 140
+    newtext = newtext[0,137] + "..."
+  end
+  client.update(newtext)
+  highestcount = tweet.id
+  File.open(PATH_PREFIX + "highest", "w") {|f| f.write(highestcount)}
+end
+
